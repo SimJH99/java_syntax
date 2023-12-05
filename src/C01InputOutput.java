aef470edcc2fa1c4dfa999a16ff859950182e134
SimJH99 <s99s4481@gmail.com>
2023-12-05 11:48:44 +0900

    first

---
+++ src/C01InputOutput.java
@@ -0,0 +1,12 @@
+public class C01InputOutput {
+    public static void main(String[] args) {
+
+        //출력
+        int a = 20;
+        //print는 출력후 줄바꿈 없음. println은 출력후 줄바꿈
+        System.out.print(a);
+        String myString = "hello world";
+        //문자열과 숫자를 하비면 문자가 된다.
+        System.out.println(myString + a);
+    }
+}
