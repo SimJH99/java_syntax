8dd978370e305857e95b6a07c2b69016bed64fc6
SimJH99 <s99s4481@gmail.com>
2023-12-05 14:08:18 +0900

    fix

--- src/C00HelloWorld.java
+++ src/C00HelloWorld.java
@@ -1,6 +1,16 @@
 public class C00HelloWorld {
     public static void main(String[] args) {
+        System.out.println("hello world");
 
+        //정수형
+        int a = 20;
+        System.out.println(a);
+
+        //문자열
+        String myString = "hello world";
+        System.out.println(myString);
+
+        //문자열과 정수를 합치면 문자열이 된다.
+        System.out.println(myString + a);
     }
-
 }
